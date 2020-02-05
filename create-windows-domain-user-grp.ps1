1d09a71cc1b0af6c411960838211e3c3c99b5c52
aiyan2 <aiyanma@gmail.com>
2020-02-05 09:32:30 -0800

    Update create-windows-domain-user-grp.ps1

--- create-windows-domain-user-grp.ps1
+++ create-windows-domain-user-grp.ps1
@@ -1,11 +1,17 @@
+#2020-0205- debug and run @166( devqa.lab), to simualte the 65535 length of grps for user allGrp
+# the max length of win-dc groups is 64, so has to create ~1000 groups 
 Import-Module ActiveDirectory
-$groups = Import-Csv ‘c:\ft\grp.csv‘
+$groups = Import-Csv 'c:\ft\grp.csv'
+#$SEED = 'AAAA50123456789012345678901234567890123456789512345678961234'
+#$SEED = 'ABAA50123456789012345678901234567890123456789512345678961234'
+$SEED = 'ANAA50123456789012345678901234567890123456789512345678961234'
 foreach ($group in $groups) {
-#New-ADGroup -Name agp$group.name -Path “CN=Domain Users,CN=Users,DC=devqa,DC=lab” -Description “Bulk” -GroupCategory Security -GroupScope Universal -Managedby BC
-
-Get-ADGroup "Domain Users" -Properties Description | New-ADGroup -Name $group.name -SamAccountName $group.name  -GroupCategory Distribution -PassThru
+#New-ADGroup -Name agp$group.name -Path ?CN=Domain Users,CN=Users,DC=devqa,DC=lab? -Description ?Bulk? -GroupCategory Security -GroupScope Universal -Managedby BC
+$gName  = $SEED + $group.name 
+Get-ADGroup "Domain Users" -Properties Description | New-ADGroup -Name $gName  -SamAccountName $gName  -GroupCategory Distribution -PassThru
 
 }
+
 
 
 ### grp.csv content
@@ -29,5 +35,7 @@ name
 27
 28
 29
+..
+50 
 ...
 ###
