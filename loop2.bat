5a9cd951ef5fc59b38ef9fc2ff07db4ebb8b1970
aiyan2 <aiyanma@gmail.com>
2020-02-14 15:42:39 -0800

    Add files via upload

---
+++ loop2.bat
@@ -0,0 +1,26 @@
+﻿@echo off
+SET DIR=%~dp0%
+SET ARGS=%*
+@echo on
+
+echo " start.."
+:start
+
+curl https://www.ubc.ca 
+curl htp://httpbin.org/启发式算法（heuristicalgorithm)是相对于最优化算法提出的。一个问题的最优算法求得该问题每个实例的最优解。启发式算法可以这样定义：一个基于直观或经验构造的算法，在可接受的花费（指计算时间和空间）下给出待解决组合优化问题每一个实例的一个可行解，该可行解与最优解的偏离程度一般不能被预计
+
+curl https://docs.google.com/document/d/1IDwcgW8pyZxwDL7Rg5EBLl3XipTPC8bsHV0eWdX-8YM/edit
+
+
+REM echo AAABAAABAAAAAAAAB2V4YW1wbGUEdGVzdAAAAQAB | certutil -decode | curl -is --data-binary @- -H 'content-type: application/dns-message'   https://dns.google/dns-query
+
+curl 9gag.com 
+
+curl bet365.com
+
+curl wwwtdd.com
+curl wwtdd.com 
+
+
+timeout 1 
+goto start
