e6cb7372894d083472ca38e6b85831400d1dcdf1
aiyan <aiyanma@gmail.com>
2020-09-15 16:00:16 -0700

    Update p1h.sh

--- p1h.sh
+++ p1h.sh
@@ -13,3 +13,11 @@ echo $counter
 sleep 1 
 done
 
+# while in one line
+
+while true; do { echo -e 'HTTP/1.1 200 OK\r\n'} | nc -l 8909; done;
+while true; do curl -x 172.18.43.64:8080 172.18.43.100;sleep 2; done
+while true; do curl -x 172.18.43.64:8080 172.18.43.100 172.18.43.100/eicar.com.1  172.18.43.100/test/badips   172.18.43.100/test/m4 -o tmp2; done
+# create files
+cmd:
+python: 
